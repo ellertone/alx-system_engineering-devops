d63621b75fc63739a89091a9632b53d23edd072c
Ellertone <ellertonemain@gmail.com>
2023-09-23 17:48:51 +0300

    Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
+# Puppet creates a file in /tmp
+
 file { '/tmp/school':
-	path    => '/tmp/school',
   ensure  => file,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love puppet',
+  content => 'I love Puppet'
 }
